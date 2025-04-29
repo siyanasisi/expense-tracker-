4de62412d7bc42c7e11a217a99ed19735180770d
Siyana Racheva <119444284+siyanasisi@users.noreply.github.com>
2025-04-29 16:57:11 +0300

    feat: update dependencies and implement routing  - Added new dependencies: axios, emoji-picker-react, moment, react-hot-toast, react-icons, and recharts. - Implemented routing in App.jsx with routes for Login, SignUp, Home, Income, and Expense pages. - Added Tailwind CSS and Google Fonts integration in index.css. - Updated vite.config.js to include Tailwind CSS plugin.

--- frontend/expense-tracker-/src/App.jsx
+++ frontend/expense-tracker-/src/App.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import{
+  BrowserRouter as Router,
+  Route,
+  Routes, 
+  Navigate,
+} from 'react-router-dom';
+
+import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
+import Home from './pages/Dashboard/Home';
+import Income from './pages/Dashboard/Income';
+import Expense from './pages/Dashboard/Expense';
 
 const App = () => {
   return (
     <div>
-      Expense Tracker
+      <Router>
+        <Routes> 
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signUp" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
